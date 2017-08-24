c179c13ef63e7f314662d7496c35a7f44715edc1
Aleksandr Mazurenko <neo.katso@gmail.com>
2017-08-24 22:29:31 +0300

    initial

--- ci-helper/deploySteps/deployHelm.groovy
+++ ci-helper/deploySteps/deployHelm.groovy
@@ -7,31 +7,31 @@ def call(image, workDir='') {
   withCredentials([file(credentialsId: p.k8sConfigName, variable: 'KUBECONFIG')]) {
     withEnv(["KUBECONFIG=${KUBECONFIG}"]) {    
         docker.withRegistry(p.dockerRegistryUrl, p.dockerRegistryCredentialsId) {
-             docker.image("helm-kubectl").inside('--sysctl net.ipv6.conf.all.disable_ipv6=1') {
+             docker.image("helm-kubectl").inside("-e HELM_HOME=${WORKSPACE}") {
 
              sh("cd ci-helper/infra/helm-${p.applicationName}/; helm lint; cd -")
              installed = sh (script: "helm ls --tiller-namespace=${p.tillerNamespace}", returnStdout: true).find(/${p.applicationName}-${env.ENV_STACK}/)
 
-                 if (!installed){ 
-                  sh(returnStdout: true, script: """
-                     helm init --client-only; 
-                     echo 'debug';
-                     helm install ci-helper/infra/helm-${p.applicationName} --name=${p.applicationName}-${env.ENV_STACK} \
-                     --set dockerImage=${p.dockerRegistryUrl.split('/')[2]}/${image} \
-                     --set ingress.domain=${env.ENV_STACK}.platform.mnscorp.net \
-                     --set ingress.cluster=${env.ENV_STACK} \
-                     --set environment=${env.ENV_STACK} \
-                     --tiller-namespace=${p.tillerNamespace};
-                     """) } else {
-                  sh(returnStdout: true, script: """
-                     cd ci-helper/infra/helm-${p.applicationName}; 
-                     helm upgrade ${p.applicationName}-${env.ENV_STACK} . \
-                     --set dockerImage=${p.dockerRegistryUrl.split('/')[2]}/${image} \
-                     --set ingress.domain=${env.ENV_STACK}.platform.mnscorp.net \
-                     --set ingress.cluster=${env.ENV_STACK} \
-                     --set environment=${env.ENV_STACK} \
-                     --tiller-namespace=${p.tillerNamespace};
-                     cd -; """)}
+             if (!installed){ 
+              sh(returnStdout: true, script: """
+                 helm init --client-only; 
+                 echo 'debug';
+                 helm install ci-helper/infra/helm-${p.applicationName} --name=${p.applicationName}-${env.ENV_STACK} \
+                 --set dockerImage=${p.dockerRegistryUrl.split('/')[2]}/${image} \
+                 --set ingress.domain=${env.ENV_STACK}.platform.mnscorp.net \
+                 --set ingress.cluster=${env.ENV_STACK} \
+                 --set environment=${env.ENV_STACK} \
+                 --tiller-namespace=${p.tillerNamespace};
+                 """) } else {
+              sh(returnStdout: true, script: """
+                 cd ci-helper/infra/helm-${p.applicationName}; 
+                 helm upgrade ${p.applicationName}-${env.ENV_STACK} . \
+                 --set dockerImage=${p.dockerRegistryUrl.split('/')[2]}/${image} \
+                 --set ingress.domain=${env.ENV_STACK}.platform.mnscorp.net \
+                 --set ingress.cluster=${env.ENV_STACK} \
+                 --set environment=${env.ENV_STACK} \
+                 --tiller-namespace=${p.tillerNamespace};
+                 cd -; """)}
               } //image
          } //registry     
     }//env
