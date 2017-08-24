37a0d6f64cd9de25266059c13c05a08f35b1e84a
Aleksandr Mazurenko <neo.katso@gmail.com>
2017-08-24 18:09:45 +0300

    initial

--- ci-helper/strategies/pr.groovy
+++ ci-helper/strategies/pr.groovy
@@ -5,7 +5,7 @@ def call(workDir='') {
 				dir(workDir){    	
 			        stage("SonarQube analysis") {
 			            stage='SonarQube analysis'
-			            load("../ci-helper/buildSteps/sonarAnalysis.groovy")(repo,workDir)
+			            load("ci-helper/buildSteps/sonarAnalysis.groovy")(repo,workDir)
 			        }
 			    }
 			    manager.addShortText("${env.BRANCH_NAME}/${getLastCommit(-1).take(7)} - sonarAnalysis only", "black", "lightblue", "1px", "grey" )
